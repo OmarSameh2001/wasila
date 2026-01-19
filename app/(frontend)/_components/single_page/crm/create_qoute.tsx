52e7283d8e11a2daa914bc6896000bb18d3e1f91
Omar Sameh <120422966+OmarSameh2001@users.noreply.github.com>
2026-01-19 04:02:29 +0200

    Make client search conditional

--- app/(frontend)/_components/single_page/crm/create_qoute.tsx
+++ app/(frontend)/_components/single_page/crm/create_qoute.tsx
@@ -543,7 +543,7 @@ export default function QuoteCreate() {
               <DynamicSearchField
                 field={{
                   key: "clientId",
-                  label: "User",
+                  label: authType === "ADMIN"?"User":"Client",
                   type: "search",
                   required: true,
                   prev: "name",
